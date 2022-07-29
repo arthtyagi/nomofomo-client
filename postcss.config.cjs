9c2c57ec87da1a6722c5554954c2b48492404a2e
Arth Tyagi <arthtyagi7@gmail.com>
2022-07-29 19:26:18 +0530

    added tailwind, eslint (airbnb), postcss

---
+++ postcss.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
+
+/*ES6 config
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export default({
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+  ],
+});
+*/
